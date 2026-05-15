5474050bf7707f781dd9e29da22f82d570f9c83b
Thore Schwier <me@thore-schwier.com>
2026-05-15 13:11:21 +0200

    Remove freeglut binaries

--- build_libs.sh
+++ build_libs.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo "cmake glew"
+echo "-------------------glew-------------------"
 
-cd ../glew-glew-2.3.1/auto
+cd libs/glew-glew-2.3.1/auto
 make all
 cd ..
 make
 
 echo "-------------------freeglut-------------------"
 
-echo "cmake freeglut"
-cd libs/freeglut-3.8.0/
+cd ../freeglut-3.8.0/
 cmake . -DFREEGLUT_COCOA=on
-echo "make"
 make
-echo "make install"
-sudo make install
