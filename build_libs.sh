7042d8aacd453fc50ea5604c6d478476f3f0ab8b
Thore Schwier <me@thore-schwier.com>
2026-05-15 12:37:04 +0200

    Add missing Makefiles

--- build_libs.sh
+++ build_libs.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-cd libs/freeglut-3.8.0/
-cmake . -DFREEGLUT_COCOA=on
-make
-sudo make install
+echo "cmake glew"
 
 cd ../glew-glew-2.3.1/build
 cmake ./cmake
+echo "make"
+make -j4
+echo "make install"
+sudo make install
+
+echo "-------------------freeglut-------------------"
+
+echo "cmake freeglut"
+cd libs/freeglut-3.8.0/
+cmake . -DFREEGLUT_COCOA=on
+echo "make"
 make
+echo "make install"
 sudo make install
