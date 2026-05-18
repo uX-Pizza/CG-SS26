0ffb5e04037d39b6e43af46109ff03c7617746ca
Thore Schwier <me@thore-schwier.com>
2026-05-18 18:06:06 +0200

    Add make install to build_libs.sh

--- build_libs.sh
+++ build_libs.sh
@@ -6,9 +6,11 @@ cd libs/glew-glew-2.3.1/auto
 make all
 cd ..
 make
+sudo make install
 
 echo "-------------------freeglut-------------------"
 
 cd ../freeglut-3.8.0/
 cmake . -DFREEGLUT_COCOA=on
 make
+sudo make install
